91b99bcfeb6e67165cb49ff968b2e3a5ac27fb34
João Lucas <joao586@hotmail.com>
2020-10-14 17:06:15 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!')
 })
 
-app.listen(port, () => {
-  console.log('Umbler listening on port %s', port)
-})
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
