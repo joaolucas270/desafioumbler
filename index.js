1e07d6b11015c68e51dc7482e3d356690ade0ef1
João Lucas <joao586@hotmail.com>
2020-10-16 06:34:58 -0300

    aosk

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use((req, res, next) => {
       next();
 });
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>")
+ })
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Umbler listening on port %s', port);
