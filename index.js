027f31149fbed1e5eb3e82ae3e8ed8b6eba52c91
João Lucas <joao586@hotmail.com>
2020-10-16 06:29:59 -0300

    um dos ultimos

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
 
 app.use((req, res, next) => {
   if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) 
@@ -8,41 +7,6 @@ app.use((req, res, next) => {
   else
       next();
 });
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://joao:backmann@mongo_bddesafionode:27017/bddesafionode', { 
-
- useNewUrlParser: true,
- useUnifiedTopology: true
-})
-.then(()=>{console.log("Mongobd conectado com sucesso!!");})
-.catch((error)=>{console.log("Houve um erro: " + error);
-})
-
-var Aluno = mongoose.Schema({
-  nome: {
-    type: String
-  },
-  idade: {
-    type: String
-  }
-})
-mongoose.model('alunos', Aluno)
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!!</h1>')
-})
-
-app.get("/", (req, res) => {
-  Aluno.find({}).then((alunos) => {
-       return res.json(alunos)
-   }).catch((erro) => {
-       return res.status(400).json({
-           error: true,
-           message: "Nenhum aluno encontrado!"
-       })
-   })
-})
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
